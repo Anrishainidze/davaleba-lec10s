f99f331437e9557ba37ddd2f32f4f824db4a28f9
Anrishainidze <123399273+Anrishainidze@users.noreply.github.com>
2023-02-27 20:58:11 +0400

    sss

---
+++ script.js
@@ -0,0 +1,66 @@
+//პირველი დავალება//
+// var income = prompt("შეიყვანე თქვენი ხელფასი")
+// if (income < 50){
+//     alert("იაფია")
+// }
+// else if (income >= 20 && income <30){
+//     alert("არცთუ ისე იაფი")
+// }
+// else if (income >=50 && income < 2500){
+//     alert("ძვირია")
+// }
+
+
+/////// მეორე დავალება//////
+// function display(){
+//     document.write("My Name Is Anri")
+// }
+
+
+
+//მესამე დავალება//
+// function display(x){
+//     let= z = x * x;
+//     console.log(z)
+// }
+// display(9)
+
+
+
+
+////მეოთხე დავალება/////
+// const country = {
+//     name: "SUPER HERO",
+//     languages: ["ქსრთული", "რუსული", "ინგლისური"],
+//     capital: {
+//       name: "ბერნ",
+//       population: 123123,
+//     },
+//     cities: [
+//       { name: "New-York", population: 123123 },
+//       { name: "Georgia", population: 1412 },
+//       { name: "Brugess", population: 345345 },
+//     ],
+//   };
+  
+//   document.write("<h3>SUPER HERO'ს გახმოვანება რა ენებზე იქნება</h3>");
+//   console.log(country.name);
+//   for (let i = 0; i < country.languages.length; i++) {
+//     document.write(country.languages[i] + " <br/>");
+//   }
+//   document.write("<h3>რომელ ქვეყნებშია ეს კინოები( და ასევე გახმოვანებაც :))</h3>");
+//   for (let i = 0; i < country.cities.length; i++) {
+//     document.write(country.cities[i].name + " <br/>");
+//   }
+
+
+
+// მეხუთე დავალება////
+//   var b = [ 15,53,22,198,10,28,16,70,33,951 ]
+//   for(i=0; i<a. length; i++){
+//     if(a[i] % 2 == 1){
+//       console.log(b [i])
+//     }
+//   }
+//   console.log(Math.max(...b))
+//   console.log(Math.min(...b))
